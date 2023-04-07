96997c67cec839f760e770d9df88cdf17dcdb9ee
jomari abejo <jomariabejo@gmail.com>
2023-04-07 21:25:35 +0800

    read csv via ArrayList

--- motorph/src/main/java/Main.java
+++ motorph/src/main/java/Main.java
@@ -1,6 +1,30 @@
+import gui.EmployeeData;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        // STOPSHIP: 4/1/2023
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+        EmployeeData employeeData = new EmployeeData();
+        String path = "src/main/csv/MotorPH Employee Data - Employee Details (4).csv";
+//        ArrayList<String[]> al = readData(path);
+//        al.forEach(outer-> {
+//            for (String inner: outer){
+//                System.out.println(inner.toString());
+//            }
+//        });
+
+        // Access Employee Details Employee Number
+
+        ArrayList<String[]> data= EmployeeData.readData2(path);
+
+        data.stream().forEach(s-> {
+            System.out.println(s[0]);
+        });
+
     }
 }
