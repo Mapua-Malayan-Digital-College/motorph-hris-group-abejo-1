4a40f45f4c104721dfb9bb8ba7c25a918796e6f8
jomari abejo <jomariabejo@gmail.com>
2023-04-09 00:54:58 +0800

    created employee classes

---
+++ motorph/src/main/java/Bank.java
@@ -0,0 +1,14 @@
+package gui;
+
+public class Bank {
+    String SSS;
+    String PHILHEALTH;
+    String PAGIBIG;
+    String TIN;
+    Bank(String sss, String philhealth, String pagibig, String tin){
+        this.SSS = sss;
+        this.PHILHEALTH = philhealth;
+        this.PAGIBIG = pagibig;
+        this.TIN = tin;
+    }
+}
