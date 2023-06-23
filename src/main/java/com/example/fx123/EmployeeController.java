0d697902368e66a8b2650aa299d3083a07153791
jomari abejo <jomariabejo@gmail.com>
2023-06-23 20:55:24 +0800

    update imports

--- src/main/java/com/example/fx123/EmployeeController.java
+++ src/main/java/com/example/fx123/EmployeeController.java
@@ -15,7 +15,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Duration;
-
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
