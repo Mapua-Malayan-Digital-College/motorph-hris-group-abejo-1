c8cf51a92e0e08d7ad966cc7c9f1ab28d095db00
jomari abejo <jomariabejo@gmail.com>
2023-06-26 02:43:29 +0800

    added salary scene(paycheck)

--- src/main/java/com/example/fx123/SceneController.java
+++ src/main/java/com/example/fx123/SceneController.java
@@ -52,4 +52,12 @@ public class SceneController {
         stage.setTitle("Leaves View");
         stage.show();
     }
+    public static void salaryScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(SalaryController.class.getResource("salary-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Salary View");
+        stage.show();
+    }
 }
